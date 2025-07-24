839a1d307e9323dfa173a43ad8cc76c9e1912e63
Daniel Wei <daniel.wei2006@hotmail.com>
2025-07-24 13:04:39 -0400

    scroll smooth behavior for navlinks

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -58,7 +58,10 @@ const Navbar = () => {
           <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
             {navLinks.map((link, index) => (
               <li key={index}>
-                <NavLink href={link.path} title={link.title} />
+                <NavLink href={link.path} title={link.title} onClick={(e) => {
+          e.preventDefault();
+          window.scrollTo({ top: 0, behavior: 'smooth' });
+        }} />
               </li>
             ))}
           </ul>
