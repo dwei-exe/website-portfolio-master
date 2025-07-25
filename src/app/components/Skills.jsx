784f567febfb1b8496d13c7e73cd63f580e25d4b
Daniel Wei <daniel.wei2006@hotmail.com>
2025-07-24 23:01:34 -0400

    fix loop repeat

--- src/app/components/Skills.jsx
+++ src/app/components/Skills.jsx
@@ -8,7 +8,7 @@ const Skills = () => {
     <div className='py-8 md:py-12 bg-white'>
     <div className='container'>
     <div className='flex overflow-hidden [mask-image:linear-gradient(to_right, bg-transparent, black, bg-transparent)]'>
-      <motion.div className='flex gap-14 pr-14 flex-none' animate={{translateX:"-50%"}} transition ={{duration:20, repeat:Loop, ease:"linear"}}>
+      <motion.div className='flex gap-14 pr-14 flex-none' animate={{translateX:"-50%"}} transition ={{duration:20, repeat: Infinity, repeatType:'loop', ease:"linear"}}>
         <Image src="/assets/skills/css.png" alt="css"/>
         <Image src="/assets/skills/figma.png" alt="figma"/>
         <Image src="/assets/skills/html.png" alt="html"/>
