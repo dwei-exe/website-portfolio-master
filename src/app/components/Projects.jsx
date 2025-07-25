dfbfc3c835d00240cbeb04fe4c44728b0dae8504
Daniel Wei <daniel.wei2006@hotmail.com>
2025-07-24 23:23:18 -0400

    removed project card dup

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -3,7 +3,6 @@ import React, { useState, useRef } from "react";
 import ProjectCard from "./ProjectCard";
 import ProjectTag from "./ProjectTag";
 import { motion, useInView } from "framer-motion";
-import ProjectCard from "./ProjectCard";
 
 const projectData =[
     {
