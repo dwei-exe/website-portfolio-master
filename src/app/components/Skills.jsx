52965adb453afbaf5ddd08d9183278bc2d3e53f5
Daniel Wei <daniel.wei2006@hotmail.com>
2025-07-24 23:55:00 -0400

    background changes and duplicate tech rows

--- src/app/components/Skills.jsx
+++ src/app/components/Skills.jsx
@@ -17,8 +17,45 @@ const Skills = () => {
           {/* Create mask container */}
           <div className="relative w-full">
             {/* Add gradient masks */}
-            <div className="absolute top-0 left-0 h-full w-[100px] bg-gradient-to-r from-[#ffffff] to-transparent z-10"></div>
-            <div className="absolute top-0 right-0 h-full w-[100px] bg-gradient-to-l from-[#ffffff] to-transparent z-10"></div>
+            <div className="absolute top-0 left-0 h-full w-[100px] bg-gradient-to-r from-[#121212] to-transparent z-10"></div>
+            <div className="absolute top-0 right-0 h-full w-[100px] bg-gradient-to-l from-[#121212] to-transparent z-10"></div>
+            
+            {/* Skills slider */}
+            <motion.div
+              className="flex gap-8 py-4"
+              animate={{
+                x: ["0%", "-50%"],
+              }}
+              transition={{
+                duration: 20,
+                ease: "linear",
+                repeat: Infinity,
+              }}
+            >
+        <div><Image src="/assets/skills/css.png" height={60} width = {60} alt="css" className='justify-items-center'/><p className='text-[#7f7f83] text-xl text-center'>CSS</p></div>
+        <div><Image src="/assets/skills/figma.png" height={60} width = {60} alt="figma" className='justify-items-center'/><p className='text-[#7f7f83] text-xl text-center'>Figma</p></div>
+        <div><Image src="/assets/skills/html.png" height={60} width = {60} alt="html" className='justify-items-center'/><p className='text-[#7f7f83] text-xl text-center'>HTML</p></div>
+        <div><Image src="/assets/skills/mongodb.png" height={60} width = {60} alt="mongodb" className='justify-items-center'/><p className='text-[#7f7f83] text-xl text-center'>MongoDB</p></div>
+        <div><Image src="/assets/skills/node.png" height={60} width = {60} alt="node" className='justify-items-center'/><p className='text-[#7f7f83] text-xl text-center'>Node.js</p></div>
+
+        {/* Second set of logos for aniumation */}
+        <div><Image src="/assets/skills/css.png" height={60} width = {60} alt="css" className='justify-items-center'/><p className='text-[#7f7f83] text-xl text-center'>CSS</p></div>
+        <div><Image src="/assets/skills/figma.png" height={60} width = {60} alt="figma" className='justify-items-center'/><p className='text-[#7f7f83] text-xl text-center'>Figma</p></div>
+        <div><Image src="/assets/skills/html.png" height={60} width = {60} alt="html" className='justify-items-center'/><p className='text-[#7f7f83] text-xl text-center'>HTML</p></div>
+        <div><Image src="/assets/skills/mongodb.png" height={60} width = {60} alt="mongodb" className='justify-items-center'/><p className='text-[#7f7f83] text-xl text-center'>MongoDB</p></div>
+        <div><Image src="/assets/skills/node.png" height={60} width = {60} alt="node" className='justify-items-center'/><p className='text-[#7f7f83] text-xl text-center'>Node.js</p></div>
+      </motion.div>
+          </div>
+        </div>
+        <h3 className="text-center text-2xl font-semibold text-white mb-8">
+          Programming Languages
+        </h3>
+        <div className="relative overflow-hidden w-full">
+          {/* Create mask container */}
+          <div className="relative w-full">
+            {/* Add gradient masks */}
+            <div className="absolute top-0 left-0 h-full w-[100px] bg-gradient-to-r from-[#121212] to-transparent z-10"></div>
+            <div className="absolute top-0 right-0 h-full w-[100px] bg-gradient-to-l from-[#121212] to-transparent z-10"></div>
             
             {/* Skills slider */}
             <motion.div
