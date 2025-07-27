51d374bd9a8c93be06a7082c316e8194370127c8
Daniel Wei <daniel.wei2006@hotmail.com>
2025-07-27 14:31:57 -0400

    projects section updates

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -8,7 +8,7 @@ const projectData =[
     {
         id: 1,
         title: "Deep CNN Arrhythmia Classifier",
-        description: "Published a paper on a this deep-CNN model that can classify 4 types of irregular heartbeats using Wavelet Scalogram Transform. It achieved 96.88% overall accuracy.",
+        description: "Published a paper on a this deep-CNN model that can classify 4 types of irregular heartbeats using Wavelet Scalogram Transform. Achieved 96.88% overall accuracy. Revealed age-training Bias on existing models with balanced datasets for medical diagnoses overlooked in traditional literature.",
         image: '/assets/projects/2.png',
         tech: ["/assets/skills/Python.png","/assets/skills/MatLab.svg","/assets/skills/TensorFlow.png"],
         tag: ["All", "Web"],
@@ -17,7 +17,7 @@ const projectData =[
     },
     {
         id: 2,
-        title: "Canadian Robotics Competition Portal",
+        title: "Canadian Robotics Competition Portal and Infra",
         description: "Full-stack developement and servicing of the official competition platform for the annual Canada-wide Robotics Competition (CRC). Front-end built in React Native with MongoDB backend. Handles peak traffic of thousands of daily requests during competition days for score tracking, match schedules, and team info.",
         image: '/assets/projects/1.png',
         tech: ["/assets/skills/React.png","/assets/skills/MongoDB.png","/assets/skills/html.png","/assets/skills/css.png","/assets/skills/Javascript.png","/assets/skills/Figma.png"],
@@ -28,12 +28,12 @@ const projectData =[
     {
         id: 3,
         title: "Robotics",
-        description: "Project 3 description",
+        description: "Programmed robot control functionalities for the Marianopolis Robotics Team at the Canadian Robotics Competition. Features include Omnidirectional Movement, Flywheel Shooter & Aiming Systems, Proportional-Derivative-Integral Controller with Quadrature Encoders for arm leveling.",
         image: '/assets/projects/3.png',
-        tech: ["/assets/skills/css.png","/assets/skills/html.png","/assets/skills/node.png"],
+        tech: ["/assets/skills/C++.png","/assets/skills/Arduino.png","/assets/skills/Onshape.png"],
         tag: ["All", "Web"],
         gitUrl: "https://github.com/dwei-exe/2025-Robot-Controls",
-        previewUrl: "https://github.com/dwei-exe/2025-Robot-Controls",
+        previewUrl: "https://cad.onshape.com/documents/a2734e4784fa8068635b3294/w/16b7b7efe2af4ba885564039/e/841d01ddace3edf723e5a0dc?renderMode=0&uiState=6875c6f51bd008323ed1c823",
     },
     {
         id: 4,
@@ -47,9 +47,9 @@ const projectData =[
     },
     {
         id: 5,
-        title: "My Personal Website",
+        title: "Artificial Intestine Mapper",
         description: "The website you're currently exploring! It's a fully responsive website for both mobile and desktop screens, so you can check out my work anywhere. Build with React, Next.js, HTML/CSS/JS and designed with Figma.",
-        image: '/assets/projects/4.png',
+        image: '/assets/projects/5.png',
         tech: ["/assets/skills/Javascript.png","/assets/skills/OnShape.png"],
         tag: ["All", "Web"],
         gitUrl: "https://github.com/dwei-exe/website-portfolio-master",
